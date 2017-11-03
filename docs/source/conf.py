32f686a6eb3be2a47741353c2d854fafaff3f463
Aaron Gonzales <agonzales@twitter.com>
2017-11-02 23:52:53 -0600

    updated to remove source file link

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -221,7 +221,7 @@ html_theme_options = {
 
     # Location of link to source.
     # Options are "nav" (default), "footer" or anything else to exclude.
-    'source_link_position': "nav",
+    'source_link_position': None,
 
     # Bootswatch (http://bootswatch.com/) theme.
     #
