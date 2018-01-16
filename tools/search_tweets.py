1f082cafb94ef98b68757fa352f28711837b9cc0
Aaron Gonzales <agonzales@twitter.com>
2018-01-16 16:15:07 -0700

    updated command-line app to use new authentication scheme with simplified command-line options

--- tools/search_tweets.py
+++ tools/search_tweets.py
@@ -6,9 +6,12 @@ import argparse
 import json
 import sys
 import logging
-from searchtweets.result_stream import ResultStream
-from searchtweets.api_utils import *
-from searchtweets.utils import *
+from searchtweets import (ResultStream,
+                          load_credentials,
+                          merge_dicts,
+                          read_configfile,
+                          write_result_stream,
+                          gen_params_from_config)
 
 logger = logging.getLogger()
 # we want to leave this here and have it command-line configurable via the
@@ -20,7 +23,7 @@ REQUIRED_KEYS = {"pt_rule", "endpoint"}
 
 
 def parse_cmd_args():
-    twitter_parser = argparse.ArgumentParser()
+    argparser = argparse.ArgumentParser()
     help_msg = """configuration file with all parameters. Far,
           easier to use than the command-line args version.,
           If a valid file is found, all args will be populated,
@@ -28,122 +31,128 @@ def parse_cmd_args():
           will overrule args found in the config,
           file."""
 
-    twitter_parser.add_argument("--credential-file",
-                                dest="credential_file",
-                                default=None,
-                                help=("Location of the yaml file used to hold "
-                                      "your credentials."))
+    argparser.add_argument("--credential-file",
+                           dest="credential_file",
+                           default=None,
+                           help=("Location of the yaml file used to hold "
+                                 "your credentials."))
 
-    twitter_parser.add_argument("--credential-file-key",
-                                dest="credential_yaml_key",
-                                default=None,
-                                help=("the key in the credential file used "
-                                      "for this session's credentials. "
-                                      "Defaults to search_tweets_api"))
+    argparser.add_argument("--credential-file-key",
+                           dest="credential_yaml_key",
+                           default=None,
+                           help=("the key in the credential file used "
+                                 "for this session's credentials. "
+                                 "Defaults to search_tweets_api"))
 
-    twitter_parser.add_argument("--config-file",
-                                dest="config_filename",
-                                default=None,
-                                help=help_msg)
+    argparser.add_argument("--env-overwrite",
+                           dest="env_overwrite",
+                           default=True,
+                           help=("""Overwrite YAML-parsed credentials with
+                                 any set environment variables. See API docs or
+                                 readme for details."""))
 
-    twitter_parser.add_argument("--account-type",
-                                dest="account_type",
-                                default=None,
-                                help="The account type you are using")
+    argparser.add_argument("--config-file",
+                           dest="config_filename",
+                           default=None,
+                           help=help_msg)
 
+    argparser.add_argument("--account-type",
+                           dest="account_type",
+                           default=None,
+                           choices=["premium", "enterprise"],
+                           help="The account type you are using")
 
-    twitter_parser.add_argument("--count-bucket",
-                                dest="count_bucket",
-                                default=None,
-                                help=("Bucket size for counts API. Options:",
-                                      "day, hour, minute (default is 'day')."))
+    argparser.add_argument("--count-bucket",
+                           dest="count_bucket",
+                           default=None,
+                           help=("""Bucket size for counts API. Options:,
+                                 day, hour, minute (default is 'day')."""))
 
-    twitter_parser.add_argument("--start-datetime",
-                                dest="from_date",
-                                default=None,
-                                help="Start of datetime window, format "
-                                     "'YYYY-mm-DDTHH:MM' (default: -30 days)")
+    argparser.add_argument("--start-datetime",
+                           dest="from_date",
+                           default=None,
+                           help="""Start of datetime window, format
+                                'YYYY-mm-DDTHH:MM' (default: -30 days)""")
 
-    twitter_parser.add_argument("--end-datetime", dest="to_date",
-                                default=None,
-                                help="End of datetime window, format "
-                                     "'YYYY-mm-DDTHH:MM' (default: most recent"
-                                     " date)")
+    argparser.add_argument("--end-datetime",
+                           dest="to_date",
+                           default=None,
+                           help="""End of datetime window, format
+                                 'YYYY-mm-DDTHH:MM' (default: most recent
+                                 date)""")
 
-    twitter_parser.add_argument("--filter-rule", dest="pt_rule",
-                                default=None,
-                                help="PowerTrack filter rule (See: http://support.gnip.com/customer/portal/articles/901152-powertrack-operators)")
+    argparser.add_argument("--filter-rule",
+                           dest="pt_rule",
+                           default=None,
+                           help="PowerTrack filter rule (See: http://support.gnip.com/customer/portal/articles/901152-powertrack-operators)")
 
-    twitter_parser.add_argument("--results-per-call", dest="results_per_call",
-                                default=100,
-                                help="Number of results to return per call "
+    argparser.add_argument("--results-per-call",
+                           dest="results_per_call",
+                           default=100,
+                           help="Number of results to return per call "
                                 "(default 100; max 500) - corresponds to "
                                 "'maxResults' in the API")
 
-    twitter_parser.add_argument("--max-results", dest="max_results",
-                                default=500,
-                                type=int,
-                                help="Maximum results to return for this "
+    argparser.add_argument("--max-results", dest="max_results",
+                           default=500,
+                           type=int,
+                           help="Maximum results to return for this "
                                 "session (defaults to 500; "
                                 "see -a option")
 
-    twitter_parser.add_argument("--max-pages",
-                                dest="max_pages",
-                                type=int,
-                                default=None,
-                                help="Maximum number of pages/api calls to "
-                                "use for this session.")
+    argparser.add_argument("--max-pages",
+                           dest="max_pages",
+                           type=int,
+                           default=None,
+                           help="Maximum number of pages/api calls to "
+                           "use for this session.")
 
-    twitter_parser.add_argument("--results-per-file", dest="results_per_file",
-                                default=0,
-                                type=int,
-                                help="Maximum tweets to save per file.")
+    argparser.add_argument("--results-per-file", dest="results_per_file",
+                           default=0,
+                           type=int,
+                           help="Maximum tweets to save per file.")
 
-    twitter_parser.add_argument("--filename-prefix",
-                                dest="filename_prefix",
-                                default=None,
-                                help="prefix for the filename where tweet "
-                                " json data will be stored.")
+    argparser.add_argument("--filename-prefix",
+                           dest="filename_prefix",
+                           default=None,
+                           help="prefix for the filename where tweet "
+                           " json data will be stored.")
 
-    twitter_parser.add_argument("--no-print-stream",
-                                dest="print_stream",
-                                action="store_false",
-                                help="disable print streaming")
+    argparser.add_argument("--no-print-stream",
+                           dest="print_stream",
+                           action="store_false",
+                           help="disable print streaming")
 
-    twitter_parser.add_argument("--print-stream",
-                                dest="print_stream",
-                                action="store_true",
-                                default=True,
-                                help="Print tweet stream to stdout")
+    argparser.add_argument("--print-stream",
+                           dest="print_stream",
+                           action="store_true",
+                           default=True,
+                           help="Print tweet stream to stdout")
 
-
-
-    twitter_parser.add_argument("--debug",
-                                dest="debug",
-                                action="store_true",
-                                default=False,
-                                help="print all info and warning messages")
-    return twitter_parser
+    argparser.add_argument("--debug",
+                           dest="debug",
+                           action="store_true",
+                           default=False,
+                           help="print all info and warning messages")
+    return argparser
 
 
 def main():
+    parser = parse_cmd_args()
     args_dict = vars(parse_cmd_args().parse_args())
     if args_dict.get("debug") is True:
         logger.setLevel(logging.DEBUG)
-
-    logger.debug(json.dumps(args_dict, indent=4))
+        logger.debug(json.dumps(args_dict, indent=4))
 
     if args_dict.get("config_filename") is not None:
         configfile_dict = read_configfile(args_dict["config_filename"])
     else:
         configfile_dict = {}
 
-
     creds_dict = load_credentials(filename=args_dict["credential_file"],
                                   account_type=args_dict["account_type"],
-                                  yaml_key=args_dict["credential_yaml_key"])
-
-
+                                  yaml_key=args_dict["credential_yaml_key"],
+                                  env_overwrite=args_dict["env_overwrite"])
 
     dict_filter = lambda x: {k: v for k, v in x.items() if v is not None}
 
@@ -169,8 +178,7 @@ def main():
     if config_dict.get("filename_prefix") is not None:
         stream = write_result_stream(rs,
                                      filename_prefix=config_dict["filename_prefix"],
-                                     results_per_file=config_dict["results_per_file"],
-                                    )
+                                     results_per_file=config_dict["results_per_file"])
     else:
         stream = rs.stream()
 
@@ -181,24 +189,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
-    # twitter_parser.add_argument("--user-name",
-    #                             dest="username",
-    #                             default=None,
-    #                             help="User name for Enterprise API access")
-
-    # twitter_parser.add_argument("--password",
-    #                             dest="password",
-    #                             default=None,
-    #                             help="Password for Enterprise API access")
-
-    # twitter_parser.add_argument("--bearer-token",
-    #                             dest="bearer_token",
-    #                             default=None,
-    #                             help="bearer token for premium API access")
-
-    # twitter_parser.add_argument("--endpoint",
-    #                             dest="endpoint",
-    #                             default=None,
-    #                             help="The API endpoint you are going to use.")
-
