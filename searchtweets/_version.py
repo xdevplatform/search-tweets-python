aafa1b26ddde249cd32014cd529c215ca44298ca
Aaron Gonzales <agonzales@twitter.com>
2018-04-16 11:18:47 -0600

    Adding __version__ to package (#31)
    * added package version information to __init__.py and storing it in _version.py
    
    * setup parses version information from searchtweets/_version.py
    
    * bumped version

---
+++ searchtweets/_version.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 Twitter, Inc.
+# Licensed under the MIT License
+# https://opensource.org/licenses/MIT
+VERSION = "1.6.0"
--- setup.py
+++ setup.py
@@ -1,4 +1,26 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 Twitter, Inc.
+# Licensed under the MIT License
+# https://opensource.org/licenses/MIT
+import re
 from setuptools import setup, find_packages
+
+def parse_version(str_):
+    """
+    Parses the program's version from a python variable declaration.
+    """
+    v = re.findall(r"\d+.\d+.\d+", str_)
+    if v:
+        return v[0]
+    else:
+        print("cannot parse string {}".format(str_))
+        raise KeyError
+
+# Our version is stored here.
+with open("./searchtweets/_version.py") as f:
+    _version_line = [line for line in f.readlines()
+                     if line.startswith("VERSION")][0].strip()
+    VERSION = parse_version(_version_line)
 
 setup(name='searchtweets',
       description="Wrapper for Twitter's Premium and Enterprise search APIs",
@@ -7,7 +29,7 @@ setup(name='searchtweets',
       long_description=open('README.rst', 'r', encoding="utf-8").read(),
       author_email='agonzales@twitter.com',
       license='MIT',
-      version='1.5.0',
+      version=VERSION,
       python_requires='>=3.3',
       install_requires=["requests", "tweet_parser", "pyyaml"],
       packages=find_packages(),
