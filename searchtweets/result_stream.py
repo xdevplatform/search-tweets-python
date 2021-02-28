d18d2cd9564147be91c4b890ce65256ef96c9c88
Igor Brigadir <igor.brigadir@gmail.com>
2021-02-28 21:46:56 +0000

    whitespace

--- searchtweets/result_stream.py
+++ searchtweets/result_stream.py
@@ -424,3 +424,4 @@ def collect_results(query, max_tweets=1000, result_stream_args=None):
                       max_tweets=max_tweets,
                       **result_stream_args)
     return list(rs.stream())
+
