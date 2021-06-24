a06d3eac2b74933ff7475a314b73335fbea6d5fb
Jim Moffitt <jmoffitt@twitter.com>
2021-06-24 12:56:14 -0600

    JM: bumping version to 1.1.

--- searchtweets/_version.py
+++ searchtweets/_version.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright 2020 Twitter, Inc.
+# Copyright 2021 Twitter, Inc.
 # Licensed under the MIT License
 # https://opensource.org/licenses/MIT
-VERSION = "1.0.7"
+VERSION = "1.1.0"
